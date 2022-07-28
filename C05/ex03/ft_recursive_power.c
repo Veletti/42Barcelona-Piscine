cba822521d44e083b994755bb2fa41d07db3bc28
Veletti <53329712+Veletti@users.noreply.github.com>
2022-07-28 18:08:02 +0200

    C05.2

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvela-fe <rvela-fe@student.42barc...>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/28 16:27:56 by rvela-fe          #+#    #+#             */
+/*   Updated: 2022/07/28 16:49:53 by rvela-fe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_recursive_power(int nb, int power)
+{
+	int	count;
+	int	aux;
+
+	aux = nb;
+	if (nb == 0 && power == 0)
+	{
+		return (1);
+	}
+	return(ft_recursive_power(nb*aux, power));
+}
+
+int	main(void)
+{
+	int	nb;
+	int	power;
+
+	nb = 2;
+	power = 5;
+	printf("%d\n", ft_recursive_power(nb, power));
+	return (0);
+}
